ec3f1b7702318fde96df7ec33f536d2a6fb7a64c
Akash Goswami <akash@fusioncharts.com>
2017-04-18 23:19:23 +0530

    Add the visualization part

---
+++ src/chart/index.js
@@ -0,0 +1,109 @@
+const d3 = require('./renderer');
+
+function chart (conf, data) {
+    let chartSVG,
+        rootG,
+        historyFlowG,
+        timelineG,
+        axisLinesG,
+        snapshotG,
+        flowG,
+        axisLines,
+        snapshots,
+        height,
+        width,
+        plotXStartPos,
+        x,
+        y,
+        yMax,
+        effectiveHeight,
+        padding;
+    
+    chartSVG = 
+        d3.select(conf.mountPoint)
+            .append('svg')
+                .attr('class', 'hf-svg')
+                .attr('version', '1.1')
+                .attr('xmlns', 'http://www.w3.org/2000/svg')
+                .attr('height', height = conf.height)
+                .attr('width', width = conf.width); 
+
+    padding = {
+        h: height * 0.05,
+        w: width * 0.05
+    };
+
+    rootG = chartSVG
+        .append('g')
+            .attr('class', 'hf-root-group')
+            .attr('transform', 'translate(' + padding.w + ', ' + padding.h + ')');
+
+    historyFlowG = rootG 
+        .append('g')
+            .attr('class', 'hf-chart-group');
+    
+    timelineG = rootG 
+        .append('g')
+            .attr('class', 'hf-timeline-group');
+
+    axisLinesG = historyFlowG
+        .append('g')
+            .attr('class', 'hf-axislines-group');
+    
+    snapshotG = historyFlowG
+        .append('g')
+            .attr('class', 'hf-snapshot-group');
+    
+    flowG = historyFlowG
+        .append('g')
+            .attr('class', 'hf-flow-group');
+
+    x = d3
+        .scaleLinear()
+        .domain([0, data.length - 1])
+        .range([0, width - 2 * padding.w]);
+
+    yMax = data
+        .reduce((acc, snapshot) => {
+            let max = snapshot.getMax();
+            if (acc < max) {
+                return max;
+            }
+            return acc;
+        }, Number.NEGATIVE_INFINITY);
+
+    y = d3
+        .scaleLinear()
+        .domain([0, yMax])
+        .range([0, effectiveHeight = height - 2 * padding.w]);
+
+    axisLines = axisLinesG
+        .selectAll('path')
+        .data(data)
+        .enter()
+        .append('path')
+            .attr('d', (d, i) => { 
+                let xVal = x(i);
+                return 'M' + xVal + ',0' + 'L' + xVal + ',' + effectiveHeight;
+            });
+
+    snapshots = snapshotG 
+        .selectAll('.hf-atomic-snapshot-g')
+        .data(data)
+        .enter()
+        .append('g')
+            .attr('class', '.hf-atomic-snapshot-g')
+            .selectAll('rect')
+            .data((d, i) => 
+                d.hunks.map(hunk => 
+                    ({ hunk: hunk, groupIndex: i })))
+            .enter()
+            .append('rect')
+                .attr('x', d => d._plotXStartPos = x(d.groupIndex - .05)) 
+                .attr('y', d => d._plotYStartPos = y(d.hunk.range[0] - 1)) 
+                .attr('width', d => x(d.groupIndex + .05) - d._plotXStartPos) 
+                .attr('height', d => y(d.hunk.range[1]) - d._plotYStartPos)
+                .style('fill', d => d.hunk.meta.color);
+}
+
+export { chart as default };
---
+++ src/chart/renderer.js
@@ -0,0 +1,4 @@
+module.exports = Object.assign({},
+    require('d3-selection'),
+    require('d3-scale')
+);
