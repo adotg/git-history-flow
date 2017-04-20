a330ac3fff90b859ed5e0877f84d36ba57fb9833
Akash Goswami <akash@fusioncharts.com>
2017-04-20 21:50:11 +0530

    Retieve flow edges from hunks

--- src/git-history-flow.js
+++ src/git-history-flow.js
@@ -1,12 +1,16 @@
 import { default as transducer } from './transducer';
 import { default as walk } from './walk';
 import { default as chart } from './chart';
+import { default as Edge } from './edge';
 
 const render = (conf, data) => {
     let walkable,
         res,
+        i,
+        l,
         transducedRes,
-        snapshots = []; 
+        snapshots = [],
+        edges = []; 
 
     transducedRes = transducer(data);
     walkable = walk(transducedRes);
@@ -17,7 +21,13 @@ const render = (conf, data) => {
         res = walkable.next();
     }
 
-    return chart(conf, snapshots);
+    for ( i = 1, l = snapshots.length; i < l; i++) {
+        edges.push(snapshots[i].hunks
+            .filter(hunk => !!hunk._clonedFrom)
+            .map(hunk => Edge.between(hunk, { index: i - 1 }, { index: i })));
+    }
+
+    return chart(conf, snapshots, edges);
 }
 
 export { render as render };
