702f72180bb46c1cc0a74cdd88ef5f3488c5af0a
Akash Goswami <akash@fusioncharts.com>
2017-04-27 23:31:31 +0530

    Pretify git log with diff changes

--- src/scripts/diff-script.sh
+++ src/scripts/diff-script.sh
@@ -4,12 +4,51 @@ TARGET_FILE="$1"
 # Saves function output
 FN_OP= 
 
+maker() {
+    local new_obj=
+    local i=
+    local list=
+    
+    new_obj="{ "
+    list=$1[@]
+    list=("${!list}")
+
+    for (( i = 0; i < ${#list[*]}; i+=2 ))
+    do
+        new_obj+=\"${list[i]}\"
+        new_obj+=": "
+        new_obj+=${list[i+1]}
+        
+        new_obj+=","
+    done
+    
+    new_obj=`echo "$new_obj" | sed 's/.$//'`
+    new_obj+=" }"
+    echo $new_obj
+}
+
+insert_at_last() {
+    local open_obj=
+
+    open_obj=`echo "$1" | sed 's/.$//'`
+    open_obj+=", "
+    open_obj+=\"$2\"
+    open_obj+=": "
+    open_obj+=$3
+    open_obj+=" }"
+    
+    echo $open_obj
+}
+
 normalize_change() {
+    local op=
     if [[ $1 == *","* ]]; then
-        FN_OP=$1
+        op=$1
     else
-        FN_OP="$1,1"
+        op="$1,1"
     fi
+    
+    echo $op
 }
 
 extract_changes() {
@@ -23,22 +62,23 @@ extract_changes() {
     deletion=`echo "$res" | sed -e 's/\-\(.*\) \(.*\)/\1/'`
     addition=`echo "$res" | sed -e 's/\(.*\)\+\(.*\)$/\2/'`
     
-    normalize_change $deletion
-    op+=$FN_OP
+    op+=`normalize_change $deletion`
     op+=","
-    normalize_change $addition
-    op+=$FN_OP
+    op+=`normalize_change $addition`
     op+="]"
     
-    FN_OP=$op 
+    echo $op 
 }
 
 process() {
     local i=
     local line_list=
     local line=
-    local op="["
+    local op=
+    local op_obj="["
+    local user=
     local list_len=
+    local args=
 
     IFS=$'\n' read -rd '' -a line_list <<<"$1"
     list_len=${#line_list[*]}
@@ -47,17 +87,20 @@ process() {
     do
         line=${line_list[$i]}
         if [[ $line =~ ^@@+ ]]; then
-            extract_changes "$line" 
-            op+=$FN_OP
-            op+=","
+            op_obj+=`extract_changes "$line"`
+            op_obj+=","
             if [ "$2" = true ] ; then
                 break
             fi
         fi
     done
+    
+    op_obj=`echo "$op_obj" | sed 's/.$//'`
+    op_obj+="]"
    
-    op=`echo "$op" | sed 's/.$//'`
-    op+="]"
+    args=("changes" "$op_obj")
+    op=`maker args`
+   
     FN_OP=$op
 }
 
@@ -77,23 +120,24 @@ diffable() {
 }
 
 # Split the string sepatated by new line character in list
-DIFF_COMMITS=`git log --pretty="%h - %s" --reverse $TARGET_FILE`
+DIFF_COMMITS=`git log --pretty="{ \"commitId\": \"%h\", \"desc\": \"%s\", \"timestamp\": \"%ci\", \"user\": { \"name\": \"%cn\", \"email\": \"%ce\" } }" --reverse wercker.yml`
 IFS=$'\n' read -rd '' -a COMMITS_LIST <<<"$DIFF_COMMITS" 
 
 for (( i = 0; i < ${#COMMITS_LIST[*]}; ++ i ))
 do
-    desc=${COMMITS_LIST[$i]}
-    COMMIT_DESC=(${desc// - / })
+    item=${COMMITS_LIST[$i]}
+    COMMIT_ID=`echo "$item" | sed -e 's/^{[[:blank:]]\"commitId\":[[:blank:]]\"\(.*\)\",[[:blank:]]\"desc\":.*/\1/'`
     
     if ((i==0))
     then
         # For the first commit, apply git log as there is nothing against which it would be diffed to
-        loggable ${COMMIT_DESC[0]} 
+        loggable $COMMIT_ID
     else
         # For for subsequent commits, apply git diff between the previous and current commit 
-        diffable ${PREV_COMMIT_DESC[0]} ${COMMIT_DESC[0]} 
+        diffable $PREV_COMMIT_ID $COMMIT_ID
     fi
-
-    PREV_COMMIT_DESC=$COMMIT_DESC
-    echo "=================================="
+    
+    OP=`insert_at_last "$item" "diff" "$FN_OP"`
+    PREV_COMMIT_ID=$COMMIT_ID
+    echo "$OP"
 done
