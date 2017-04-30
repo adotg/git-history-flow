7857090c63aa7ebdc670c1b065fa679f77692b63
Akash Goswami <akash@fusioncharts.com>
2017-04-30 15:50:17 +0530

    Fix the broken unit test during to date parsing

--- src/transducer/index.spec.js
+++ src/transducer/index.spec.js
@@ -5,10 +5,10 @@ import { default as transducer } from '../transducer';
 import { default as Snapshot } from '../snapshot';
 
 const data = [
-    { diff: { changes: [0, 0, 1, 24] }, user: { email: 'a@domain.com' } },
-    { diff: { changes: [19, 1, 16, 1] }, user: { email: 'b@domain.com' } },
-    { diff: { changes: [12, 0, 13, 5] }, user: { email: 'c@domain.com' } },
-    { diff: { changes: [12, 0, 13, 3] }, user: { email: 'd@domain.com' } }
+    { diff: { changes: [0, 0, 1, 24] }, user: { email: 'a@domain.com' }, "timestamp": "2017-03-01 20:54:47 +0530" },
+    { diff: { changes: [19, 1, 16, 1] }, user: { email: 'b@domain.com' }, "timestamp": "2017-03-01 23:54:47 +0530" },
+    { diff: { changes: [12, 0, 13, 5] }, user: { email: 'c@domain.com' }, "timestamp": "2017-03-02 23:54:47 +0530" },
+    { diff: { changes: [12, 0, 13, 3] }, user: { email: 'd@domain.com' }, "timestamp": "2017-03-03 23:54:47 +0530" }
 ];
 
 
