3e4123cb56cd8d1be463fc47e19643b96b78acd0
Akash Goswami <akash@fusioncharts.com>
2017-05-15 00:17:39 +0530

    Implement focus action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 const 
     all = {},
     changeMode = all.changeMode = (mode) => ({ type: 'CHANGE_MODE', payload: { mode: mode } }),
-    changeXType = all.changeXType = (type) => ({ type: 'CHANGE_X', payload: { type: type } });
+    changeXType = all.changeXType = (type) => ({ type: 'CHANGE_X', payload: { type: type } }),
+    focus = all.focus = (index) => ({ type: 'FOCUS', payload: { index: index } });
 
-export { changeMode, changeXType , all};
+export { changeMode, changeXType, focus, all};
